d1b05c65897177cc0f085aadb7ac32e870df6775
miri993 <mirianacataldi@gmail.com>
2022-07-27 15:04:52 +0200

    Merge primo uau

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './angular.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { MatToolbarModule } from '@angular/material/toolbar';
-import {  } from '@angular/material';
 
 @NgModule({
   declarations: [
